a4697673b5f13bc27cad86419d1f9a91655753fd
Ciccio1969 <92854276+Ciccio1969@users.noreply.github.com>
2021-12-01 21:59:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    char a=0;
+    cin >> a;
+    Vero_o_Falso (a);
+
+    if ((a >= 65 and a <= 90) or (a >= 97 and a <=122)){
+        cout << Trasformazione (a) << endl;
+    }else{
+        cout << "errore" << endl;
+    }
+
+    return 0;
+
 }
